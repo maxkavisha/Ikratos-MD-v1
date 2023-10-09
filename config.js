71ef2e6664fda32a3d55af7fc4bff57801b28cdc
kavisha <147311644+maxkavisha@users.noreply.github.com>
2023-10-09 16:27:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -53,10 +53,10 @@ global.ppulsa = '085736178354' //Nomor SimCard Yang Kamu Pake
 global.psaweria = 'https://saweria.com/XiaoYan021' //Link Saweriamu Kalo Nggada Ketik - aja
 
 /*============== NOMOR ==============*/
-global.nomorbot = '68673045381' //Nomor Bot
-global.nomorown = '923470027813' //Nomor Owner
+global.nomorbot = '94711584565' //Nomor Bot
+global.nomorown = '94711584565' //Nomor Owner
 global.namebot = 'Ikratos-MD-v1' //Nama Bot
-global.nameown = 'ᴀʜᴍᴀᴅ ᴀʟɪ🇵🇰' // Nama Owner
+global.nameown = 'RDK BOT' // Nama Owner
 
 
 /*============== CHATGPT ==============*/
